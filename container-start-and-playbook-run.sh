9846321f89cb8aa40f52d25f24557f7d9752903a
Arne Rossmann <arne.rossmann@capgemini.com>
2020-08-19 21:58:51 +0200

    initial commit

---
+++ container-start-and-playbook-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DOCKER_CONTAINER_NAME="ansible-test"
+
+cd docker && docker build -t myubuntu .
+
+docker run -ti --privileged --name $DOCKER_CONTAINER_NAME -d -p 5000:22 myubuntu
+
+cd ../ansible && ansible-playbook -i env/local_docker myplaybook.yml -vvv
+docker stop $DOCKER_CONTAINER_NAME
+
+docker rm $DOCKER_CONTAINER_NAME
