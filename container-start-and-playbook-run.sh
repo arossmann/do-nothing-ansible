892c33b4536be831f7eadee44f318e51d5183a78
Arne Rossmann <arne.rossmann@capgemini.com>
2020-08-19 22:10:36 +0200

    added a bit of fun :-)

--- container-start-and-playbook-run.sh
+++ container-start-and-playbook-run.sh
@@ -6,7 +6,12 @@ cd docker && docker build -t myubuntu .
 
 docker run -ti --privileged --name $DOCKER_CONTAINER_NAME -d -p 5000:22 myubuntu
 
+# regular version
 cd ../ansible && ansible-playbook -i env/local_docker myplaybook.yml -vvv
+
+# fun version
+#cd ../ansible && ansible-playbook -i env/local_docker my_fun_playbook.yml -vvv
+
 docker stop $DOCKER_CONTAINER_NAME
 
 docker rm $DOCKER_CONTAINER_NAME
